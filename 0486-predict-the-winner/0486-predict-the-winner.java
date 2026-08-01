8006e072620fcff4450dc70c3efee1c995097495
Tushar Yadav <tusharydv08@gmail.com>
2026-08-01 12:26:12 +0530

    Time: 0 ms (100%), Space: 43 MB (23.56%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean predictTheWinner(int[] nums) {
+        int n=nums.length;
+        Integer[][] memo = new Integer[n][n];
+
+        return maxScoreDiff(nums,0,n-1,memo)>=0;
+    }
+
+    public static int maxScoreDiff(int[] nums, int s, int e, Integer[][] memo){
+        if(s==e){
+            return nums[s];
+        }
+
+        if(memo[s][e]!=null){
+            return memo[s][e];
+        }
+
+        int pickLeft = nums[s] - maxScoreDiff(nums,s+1,e,memo);
+        int pickRight = nums[e] - maxScoreDiff(nums,s,e-1,memo);
+
+        return memo[s][e]=Math.max(pickLeft,pickRight);
+    }
+}
